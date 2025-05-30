bc918b00af11d492d88220f030b22f1f31c1a0b0
kauandelix <kmoraes360@gmail.com>
2025-05-30 19:38:54 -0300

    feature

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import time
+
+VIDA_MAXIMA = 100
+ENERGIA_MAXIMA = 100
+TAMANHO_MOCHILA = 6
+
 # Mensagem inicial - Solicita nome do jogador
 jogador = str(input("""
 ======================================================================
@@ -6,6 +12,9 @@ Este é um jogo de sobrevivência onde você precisa coletar recursos,
 enfrentar desafios e voltar para casa.
 
 Digite seu nome: """))
-print("""Boa Sorte!
+print(f"""Boa Sorte {jogador}!
 ======================================================================
 """)
+
+time.sleep(2.5)
+
