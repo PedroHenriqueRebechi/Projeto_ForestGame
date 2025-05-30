ed09142a6a9da3fd5510f5ea20bc9adffbd5ddee
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2025-05-30 09:26:13 -0300

    feat: Mensagem inicial e pede nome do jogador

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("testando")
+# Mensagem inicial - Solicita nome do jogador
+jogador = str(input("""
+======================================================================
+SEJA BEM-VINDO AO FOREST GAME - SOBREVIVENDO NA FLORESTA!
+Este é um jogo de sobrevivência onde você precisa coletar recursos,
+enfrentar desafios e voltar para casa.
+
+Digite seu nome: """))
+print("""Boa Sorte!
+======================================================================
+""")
