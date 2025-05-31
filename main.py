ad3422af92db19acc6f40795fe1ce4364377e82a
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2025-05-30 22:15:18 -0300

    feat: Criando caminhos possíveis

--- main.py
+++ main.py
@@ -10,35 +10,32 @@ def criar_pausa():
         print(".", end="")
         time.sleep(0.5)
 
-
 def enviar_introducao():
     # Mensagem inicial - Solicita nome do jogador
-    nome = str(
-        input(
-            """
-======================================================================
+    nome = str(input("""
+==============================================================================
 SEJA BEM-VINDO AO FOREST GAME - SOBREVIVENDO NA FLORESTA!
-Este é um jogo de sobrevivência onde você precisa coletar recursos,
-enfrentar desafios e voltar para casa.
+Este é um jogo de sobrevivência onde você precisa coletar recursos, enfrentar 
+desafios e voltar para casa. Você começa o jogo com vida e energia máxima,
+e ao decorrer do jogo você poderá restaurar esses atributos comendo e descansando.
 
-Digite seu nome: """
-        )
-    )
-    print(
-        f"""
+Digite seu nome: """))
+    print(f"""
 Boa Sorte {nome}!
-======================================================================"""
-    )
+=============================================================================""")
 
     criar_pausa()
     return nome
-
 
 def mostrar_atributos(vida, energia, mochila):
     print(f"=== Vida: {vida} ===")
     print(f"=== Energia: {energia} ===")
     print(f"=== Mochila: {mochila} ===\n")
 
+def mostrar_opcoes():
+    opcao = int(input("""
+Digite sua opção:
+"""))
 
 while True:
 
@@ -50,22 +47,41 @@ while True:
     jogador = enviar_introducao()
 
     # 1ª Situação
-    print(
-        "\nVocê acorda em uma floresta densa e escura, não sabendo onde está e o que fazer. Você encontra ao seu redor uma mochila e prontamente pega ela.\n"
-    )
-    mostrar_atributos(vida, energia, mochila)
+    print("\nVocê acorda em uma floresta densa e escura, não sabendo onde está e o que fazer. Você encontra ao seu redor uma mochila e prontamente pega ela\n")
+    # mostrar_atributos(vida, energia, mochila)
     criar_pausa()
 
-    
-    opcao = int(
-        input(
-            """
+    opcao = int(input("""
 [1] Olhar dentro da mochila
 [2] Começa a explorar a floresta
 [3] Grita por ajuda
 
-Digite sua opção: """
-        )
-    )
+[4] Ver atributos              
+[5] Sair do jogo 
 
-    # if opcao == 1:
+Digite sua opção: """))
+
+    if opcao == 1:
+        print('\nVocê olha dentro da mochila e encontra duas maças, elas parecem saborosas')
+        mochila.append('maçã')
+        mochila.append('maçã')
+        continue
+
+    elif opcao == 2:
+        print('\nVocê começa a andar pela floresta, a princípio sem rumo algum.')
+        criar_pausa()
+        print('Depois de tanto andar, você finalmente encontra algo, pegadas de algum animal estão indo para uma caverna')
+        continue
+
+    elif opcao == 3:
+        continue
+
+    elif opcao == 4:
+        mostrar_atributos(vida, energia, mochila)
+
+    elif opcao == 5:
+        print('Até logo!')
+        break
+    
+
+        
