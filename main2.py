8e2cc1cc2a686c7739ce4cc2edd811c8585bceda
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2025-06-02 21:20:04 -0300

    Função explorar

--- main2.py
+++ main2.py
@@ -5,7 +5,17 @@ VIDA_MAXIMA = 100
 ENERGIA_MAXIMA = 100
 PONTUACAO_MAXIMA = 100
 TAMANHO_MOCHILA = 6
-vida = 100
+
+vida = VIDA_MAXIMA
+energia = ENERGIA_MAXIMA
+pontuacao = 0
+mochila = []
+
+animais = {
+    'lobo': {'dano': 10},
+    'cobra': {'dano': 5},
+    'urso': {'dano': 15}
+}
 
 def criar_pausa():
     for i in range(3):
@@ -27,16 +37,39 @@ Boa Sorte {nome}!
     criar_pausa()
     return nome
 
-def mostrar_atributos(vida, energia, pontuacao,mochila):
+def mostrar_atributos(vida, energia, pontuacao, mochila):
     print(f"\nVida: {vida} | Energia: {energia} | Pontuação: {pontuacao} | Mochila: {mochila}\n")
 
-def explorar():
+def explorar(vida):
     print('\nVocê decide explorar a floresta e procurar por alguém')
     criar_pausa()
     print('\nDepois de andar por um tempo, você finalmente encontra algo. Existem pegadas indo para uma caverna, você se aproxima e ...')
-    print('\nVocê é atacado por um lobo selvagem e perde 10 pontos de vida!')
-    vida -= 10
-    
+    print('Você é atacado por um lobo selvagem e perde 10 pontos de vida!')
+    dano = 10
+    return vida - dano
 
 nome = enviar_introducao()
 
+while True:
+    mostrar_atributos(vida, energia, pontuacao, mochila)
+    print("Escolha sua ação:")
+    print("(1) Buscar comida")
+    print("(2) Montar abrigo")
+    print("(3) Explorar a floresta")
+    print("(4) Usar item da mochila")
+    acao = input("Digite o número da ação desejada: ")
+
+    if acao == '1':
+        break
+        #buscar_comida()
+    elif acao == '2':
+        break
+        #montar_abrigo()
+    elif acao == '3':
+        explorar(vida)
+        
+    elif acao == '4':
+        #usar_item()
+        break
+    else:
+        print("Ação inválida.")
